17291e6db15e3b8b7b2a3a46768c1ec718502b2f
imbaoss <baole6989@gmail.com>
2017-06-30 18:16:36 -0700

    Add files for clock widget

--- Widget.jsx
+++ Widget.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Clock from './frontend/clock';
 
-const Widget = () => (<h1>Widget App Here!</h1>)
+const Widget = () => (
+  <div>
+    <h1>Widget App Here!</h1>
+    <Clock />
+  </div>
+)
 
 export default Widget;
