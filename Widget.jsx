9f70de88d93cec4275e4e36342b00198b6b78c3a
imbaoss <baole6989@gmail.com>
2017-07-03 00:26:44 -0700

    Add autocomplete input field

--- Widget.jsx
+++ Widget.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Clock from './frontend/clock';
 import Weather from './frontend/weather';
+import Autocomplete from './frontend/autocomplete';
+
+const names = ["Stacy", "Jess", "Jim", "James", "Julie"];
 
 const Widget = () => (
   <div>
     <Clock />
     <Weather />
+    <Autocomplete props={ names }/>
   </div>
 )
 
---
+++ frontend/autocomplete.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Autocomplete extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputVal: ""
+    }
+
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleUpdate(e) {
+    debugger
+    this.setState({ inputVal: e.currentTarget.value })
+  }
+
+  render() {
+    return(
+      <div>
+        <input onChange={this.handleUpdate}></input>
+      </div>
+    )
+  }
+}
+
+export default Autocomplete;
